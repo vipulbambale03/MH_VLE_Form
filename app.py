5b844fcfc6294a1e1a6b05b663fe5a7fe7081780
vipulbambale03 <bambale.vipul@gmail.com>
2025-05-30 18:57:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mysql.connector
 from dotenv import load_dotenv
 
 load_dotenv()
-
+port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
 
 # Database connection
@@ -539,4 +539,4 @@ def update_record():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port)
